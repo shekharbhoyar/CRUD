18e90d288f8e273488675c166069a64024061ac5
chandrashekhar <shekharbhoyar10@gmail.com>
2025-03-05 23:01:48 +0530

    routes added

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,12 @@
+import { Routes } from "react-router-dom";
 import Home from "./components/Home";
 
 function App() {
   return (
     <>
-      <Home />
+      <Routes>
+        <Home />
+      </Routes>
     </>
   );
 }
