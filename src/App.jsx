bdd72ec4e6b5affd6a1ca4105493859b0eabb9bd
chandrashekhar <shekharbhoyar10@gmail.com>
2025-03-05 23:00:44 +0530

    bootstrap config

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,9 @@
+import Home from "./components/Home";
+
 function App() {
   return (
     <>
-      <div>
-        <h1 className="text-3xl font-bold underline text-blue-500">
-          Hello world!
-        </h1>
-      </div>
+      <Home />
     </>
   );
 }
